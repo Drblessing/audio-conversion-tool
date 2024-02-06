7b559013071a7980d43b6043727f12e0a7138cd8
Danny Blessing <drblessing@gmail.com>
2024-02-06 01:06:01 -0800

    docs and tkinter start

--- tkinter_tinkering.py
+++ tkinter_tinkering.py
@@ -1,5 +1,7 @@
 import tkinter as tk
+from tkinter import filedialog  # Import the filedialog module
 import sys
+import eyed3  # Import the eyed3 library
 
 
 def on_button_click():
@@ -11,12 +13,20 @@ def on_close_click(root):
     root.destroy()
 
 
+def browse_file():
+    file_path = filedialog.askopenfilename(filetypes=[("MP3 Files", "*.mp3")])
+    if file_path:
+        print(f"Selected file: {file_path}")
+
+
 def run_gui():
     root = tk.Tk()
     root.title("Simple Tkinter Example")
     tk.Label(root, text="Hello, Tkinter!").pack()
     tk.Button(root, text="Close", command=lambda: on_close_click(root)).pack()
     tk.Button(root, text="Click me!", command=on_button_click).pack()
+    tk.Button(root, text="Open File", command=browse_file).pack()
+
     root.mainloop()
 
 
