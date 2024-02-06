a91fd439a845aef94fadeb3722a7a5cc361a3067
Danny Blessing <drblessing@gmail.com>
2024-02-06 14:33:37 -0800

    id3_testing

--- id3_testing.py
+++ id3_testing.py
@@ -1,6 +1,11 @@
 import eyed3
+from eyed3 import id3
 
-audiofile = eyed3.load("assets/motivational-symphony-music.mp3")
+from eyed3.id3 import ID3_V1_1
+
+
+audiofile = eyed3.load("assets/motivational-symphony-music.mp3", tag_version=ID3_V1_1)
+audiofile.initTag(version=ID3_V1_1)
 audiofile.tag.artist = "Token Entry"
 audiofile.tag.album = "Free For All Comp LP"
 audiofile.tag.album_artist = "Various Artists"
