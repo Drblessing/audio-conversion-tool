3542fb3a4dec7b7c3d76a40803cf4b0f8d180077
Danny Blessing <drblessing@gmail.com>
2024-02-19 19:37:55 -0800

    playlist_into_album

--- flac_to_mp3.py
+++ flac_to_mp3.py
@@ -211,6 +211,52 @@ def set_genre_for_artist_directory(file_path: Path | str, genre: str):
         print(f"Setting genre for album {album_dir.name}")
         set_genre_for_album_mp3(album_dir, genre)
 
+def organize_playlist_into_album(file_path: Path | str, playlist_artist: str = "Various Artists",playlist_name : str, album_name : str, genre : str = "Various", album_art :str = 'cover.jpg'):
+    """Organize a playlist into an album.
+    This will convert all flac files in the directory to mp3.
+    The bitrate is set to 320 kbps.
+    The flac metadata will be copied to the mp3 file, as well.
+    The mp3 metadta will be cleaned up, and the genre set to the genre of the album.
+    The song names will be cleaned up, to format <track number>. <artist> - <song name>.mp3
+    The album name will also be cleaned up to <album name>
+    """
+
+    # Fix album name
+    # Get album path
+    album_path = Path(file_path)
+    # Get album name
+    album_name = album_path.name
+    # Remove brackets
+    album_name = re.sub(r"\[.*?\]", "", album_name)
+    # Strip whitespace
+    album_name = album_name.strip()
+    # Split album name by " - "
+    album_name = album_name.split(" - ")[1]
+    # Strip whitespace
+    album_name = album_name.strip()
+    # Get new album path
+    new_album_path = album_path.parent / album_name
+    # Move album to new path
+    album_path = album_path.rename(new_album_path)
+
+    # Convert all flac files to mp3
+    flac_files = [flac_file for flac_file in album_path.glob("*.flac")]
+    for flac_file in flac_files:
+        flac_to_mp3(flac_file, genre)
+
+    # Delete all flac files
+    for flac_file in flac_files:
+        flac_file.unlink()
+
+    # Set the album artist to Various Artists
+    album_path = Path(file_path)
+    # Get all mp3 files in the album
+    mp3_files = [mp3_file for mp3_file in album_path.glob("*.mp3")]
+    for mp3_file in mp3_files:
+        set_album_artist(mp3_file, playlist_artist)
+        set_album(mp3_file, album_name)
+        set_genre_mp3(mp3_file, genre)
+        set_playlist(mp3_file, playlist_name)
 
 if __name__ == "__main__":
     test_album = "a"
